2d7c3e527b193e8dcba4d153c6ccd93da5a9befb
GuyBarkay <53431687+GuyBarkay@users.noreply.github.com>
2020-04-14 12:36:46 +0300

    Delete 2_Coinflip_deploy.js

--- migrations/2_Coinflip_deploy.js
+++
@@ -1,5 +0,0 @@
-const Coinflip = artifacts.require("Coinflip");
-
-module.exports = function(deployer) {
-  deployer.deploy(Coinflip).then(function() { console.log("CONTRACT ADDRESS IS "+Coinflip.address)});
-};
